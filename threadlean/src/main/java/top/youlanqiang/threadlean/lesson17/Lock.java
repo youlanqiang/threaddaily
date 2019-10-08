d9e6aadf9030382a673ebb09ab6b7ecb2adec4a1
youlanqiang <youlanqiang@hotmail.com>
2019-10-08 09:43:18 +0800

    完成读写锁学习

---
+++ threadlean/src/main/java/top/youlanqiang/threadlean/lesson17/Lock.java
@@ -0,0 +1,11 @@
+package top.youlanqiang.threadlean.lesson17;
+
+public interface Lock {
+
+    //获取显示锁，没有获取锁的线程将被阻塞
+    void lock() throws InterruptedException;
+
+    //释放获得的锁
+    void unlock();
+
+}
