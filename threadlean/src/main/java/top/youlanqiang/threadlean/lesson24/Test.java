dba16640eb4a6d9dc2a7e2ee8aef0b007450c053
youlanqiang <youlanqiang@hotmail.com>
2019-10-11 17:07:33 +0800

    完成Thread-Per-Message模式

---
+++ threadlean/src/main/java/top/youlanqiang/threadlean/lesson24/Test.java
@@ -0,0 +1,10 @@
+package top.youlanqiang.threadlean.lesson24;
+
+import java.io.IOException;
+
+public class Test {
+
+    public static void main(String[] args) throws IOException {
+        new ChatServer().startServer();
+    }
+}
