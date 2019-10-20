6dfc3db135b07d4533dfd40eb61e8e798954c12a
youlanqiang <youlanqiang@hotmail.com>
2019-10-20 18:57:20 +0800

    完成线程上下文设计模式

---
+++ threadlean/src/main/java/top/youlanqiang/threadlean/lesson21/ActionContext.java
@@ -0,0 +1,28 @@
+package top.youlanqiang.threadlean.lesson21;
+
+public class ActionContext {
+
+    private static final ThreadLocal<Context> context = ThreadLocal.withInitial(Context::new);
+
+    public static Context get(){
+        return context.get();
+    }
+
+
+    //每一个线程都有一个独立的Context实例
+    static class Context{
+
+        //在Context中的其它成功
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+
+}
---
+++ threadlean/src/main/java/top/youlanqiang/threadlean/lesson21/ApplicationContext.java
@@ -0,0 +1,22 @@
+package top.youlanqiang.threadlean.lesson21;
+
+/**
+ * 程序上下文，整个程序应该只能出现一个
+ */
+public class ApplicationContext {
+
+    private ApplicationContext(){ }
+
+    /**
+     * 使用Holder的方式实现单例
+     */
+    private static class Holder{
+        private static ApplicationContext instance = new ApplicationContext();
+    }
+
+
+    public static ApplicationContext getContext(){
+        return Holder.instance;
+    }
+
+}
